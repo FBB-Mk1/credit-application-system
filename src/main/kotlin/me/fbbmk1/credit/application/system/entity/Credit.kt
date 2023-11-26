b38bb4a92ea1aaee8253a654df83f891afeca33e
fabin <fabiobatista@gmail.com>
2023-11-26 18:03:15 -0300

    Classes de Modelo

--- src/main/kotlin/me/fbbmk1/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/me/fbbmk1/credit/application/system/entity/Credit.kt
@@ -1,16 +1,18 @@
 package me.fbbmk1.credit.application.system.entity
 
+import jakarta.persistence.*
 import me.fbbmk1.credit.application.system.enumerator.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
 class Credit (
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null,
-    val id: Long? = null
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated @Column(nullable = false) val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
--- src/main/kotlin/me/fbbmk1/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/fbbmk1/credit/application/system/entity/Customer.kt
@@ -1,14 +1,20 @@
 package me.fbbmk1.credit.application.system.entity
 
-import java.net.Inet4Address
+import jakarta.persistence.Entity
+import jakarta.persistence.*
 
+
+@Entity
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String = "",
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit>,
-    val id: Long? = null
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String = "",
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
+        cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
+        mappedBy = "customer"
+    ) var credits: List<Credit>,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
